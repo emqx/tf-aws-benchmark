37978b4dec8f7bb3bf129f612e87e051df6a015e
mengzhenyu <mengzhenyu1990@163.com>
2021-09-16 15:47:32 +0800

    chore(autotest): add webhook service

---
+++ modules/webhook/variables.tf
@@ -0,0 +1,28 @@
+variable "namespace" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "subnet_ids" {
+    type = list(string)
+}
+
+variable "sg_ids" {
+    type = list(string)
+}
+
+variable "private_key" {
+    type = string
+}
+
+variable "associate_public_ip_address" {
+    type = bool
+}
+
