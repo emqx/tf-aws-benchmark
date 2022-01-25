5a2b1375e6e47f5b33871d8aad141fdc2d6f95aa
mengzhenyu <mengzhenyu1990@163.com>
2022-01-25 16:40:10 +0800

    chore(autotest): update mongo and redis public ip

--- services/mongodb/outputs.tf
+++ services/mongodb/outputs.tf
@@ -2,3 +2,8 @@ output "mongo_private_ips" {
   description = "private ip of mongo"
   value       = module.mongo.private_ip
 }
+
+output "emqx_public_ips" {
+  description = "public ip of ec2 for each project"
+  value       = join(",", module.mongo.public_ip)
+}
--- services/redis/outputs.tf
+++ services/redis/outputs.tf
@@ -2,3 +2,8 @@ output "redis_private_ips" {
   description = "private ip of redis"
   value       = module.redis_ec2.private_ip
 }
+
+output "redis_public_ips" {
+  description = "public ip of ec2 for each project"
+  value       = join(",", module.redis_ec2.public_ip)
+}
