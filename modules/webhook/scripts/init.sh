17b5d33e823f90f011fd1e1d652e795b485290d0
mengzhenyu <mengzhenyu1990@163.com>
2022-02-21 10:02:51 +0800

    update webhook server

--- modules/webhook/scripts/init.sh
+++ modules/webhook/scripts/init.sh
@@ -7,4 +7,5 @@ sudo apt-get -y install openjdk-11-jre-headless
 # Install webhook
 wget https://github.com/emqx/emqx-svt-web-server/releases/download/web-server-1.0/svtserver-0.0.1.jar
 nohup java -jar svtserver-0.0.1.jar > log.file 2>&1 &
+sleep 10
 ps aux | grep java
